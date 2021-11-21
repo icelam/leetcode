a9d2aca8daf9a4da3fe2b98120187e0cf1a0ae1f
Ice Lam <pinky.yy.lam@gmail.com>
2021-11-21 17:22:58 +0800

    feat: 485 - Max Consecutive Ones

---
+++ src/485-findMaxConsecutiveOnes.ts
@@ -0,0 +1,17 @@
+function findMaxConsecutiveOnes(nums: number[]): number {
+  let max = 0;
+  let count = 0;
+
+  for(const n of nums) {
+   if (n === 1) {
+     count++;
+   } else {
+     if (count > max) {
+       max = count;
+     }
+     count = 0;
+   }
+  }
+
+  return count > max ? count: max;
+};
