0f945410da538295d11433eb87afbbbd8d0f4e77
Ice Lam <pinky.yy.lam@gmail.com>
2021-12-14 00:37:49 +0800

    feat: 1446 - consecutive characters

---
+++ src/1446-maxPower.ts
@@ -0,0 +1,19 @@
+function maxPower(str: string): number {
+  let previousCharacter = '';
+  let currentCount = 0;
+  let maxCount = 0;
+
+  for (const character of str) {
+    if (previousCharacter !== character) {
+      if (currentCount > maxCount) {
+        maxCount = currentCount;
+      }
+      currentCount = 0;
+      previousCharacter = character;
+    }
+
+    currentCount++;
+  }
+
+  return currentCount > maxCount ? currentCount : maxCount;
+};
