a41b2c22c706a4db5fbbac800784c5b562d9ebca
Ice Lam <pinky.yy.lam@gmail.com>
2022-02-07 16:37:34 +0800

    feat: 389 - find the difference

---
+++ src/389-findTheDifference-v2.ts
@@ -0,0 +1,16 @@
+function findTheDifference(s: string, t: string): string {
+  let sSum = 0;
+  let xSum = 0;
+
+  for (let i = 0; i < Math.max(s.length, t.length); i++) {
+    if (i < s.length) {
+      sSum ^= s.charCodeAt(i);
+    }
+
+    if (i < t.length) {
+      xSum ^= t.charCodeAt(i);
+    }
+  }
+
+  return String.fromCharCode(sSum^xSum);
+};
---
+++ src/389-findTheDifference.ts
@@ -0,0 +1,15 @@
+function findTheDifference(s: string, t: string): string {
+  let sum = 0;
+
+  for (let i = 0; i < Math.max(s.length, t.length); i++) {
+    if (i < s.length) {
+      sum -= s.charCodeAt(i);
+    }
+
+    if (i < t.length) {
+      sum += t.charCodeAt(i);
+    }
+  }
+
+  return String.fromCharCode(sum);
+};
