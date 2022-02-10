0d01ad5a2814a318a9055e0473937bce7cbd45b5
Ice Lam <pinky.yy.lam@gmail.com>
2022-02-10 20:50:39 +0800

    feat: 80 - remove duplicates from sorted array ii

---
+++ src/80-removeDuplicates.ts
@@ -0,0 +1,19 @@
+function removeDuplicates(nums: number[]): number {
+  let length = nums.length;
+  let current = 2;
+
+  while(nums[current] !== undefined) {
+    const value = nums[current];
+
+    if (nums[current - 1] === nums[current] && nums[current - 2] === nums[current]) {
+      for (let i = current; i < length; i++) {
+        nums[i] = nums[i + 1];
+      }
+      length--;
+    } else {
+      current++;
+    }
+  }
+
+  return length;
+};
