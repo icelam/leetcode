2eb53172e5924969c3ffff546a3ff345a13ae68f
Ice Lam <pinky.yy.lam@gmail.com>
2022-03-07 14:24:33 +0800

    feat(java): 1672 - richest customer wealth

---
+++ solutions/Java/[1672_Easy] Richest Customer Wealth/maximumWealth.java
@@ -0,0 +1,17 @@
+class Solution {
+  public int maximumWealth(int[][] accounts) {
+    int maxWealth = 0;
+
+    for (int[] account: accounts) {
+      int sum = 0;
+
+      for (int wealth: account) {
+        sum += wealth;
+      }
+
+      maxWealth = Math.max(maxWealth, sum);
+    }
+
+    return maxWealth;
+  }
+}
