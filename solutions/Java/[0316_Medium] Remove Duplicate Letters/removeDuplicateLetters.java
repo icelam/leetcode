c4871163b0612faef6cc53732db95d899f8772d0
Ice Lam <pinky.yy.lam@gmail.com>
2022-03-18 18:50:21 +0800

    feat(java): 316 - remove duplicate letters

---
+++ solutions/Java/[0316_Medium] Remove Duplicate Letters/removeDuplicateLetters.java
@@ -0,0 +1,36 @@
+class Solution {
+  public String removeDuplicateLetters(String s) {
+    Stack<Character> stack = new Stack();
+    int[] count = new int[26];
+    boolean[] added = new boolean[26];
+
+    for (int i = 0; i < s.length(); i++) {
+      count[s.charAt(i) - 'a']++;
+    }
+
+    for (int i = 0; i < s.length(); i++) {
+      if (!added[s.charAt(i) - 'a']) {
+        while (
+          !stack.empty() && stack.peek() - 'a' >= s.charAt(i) - 'a'
+          && count[stack.peek() - 'a'] > 1
+        ) {
+          char removed = stack.pop();
+          added[removed - 'a'] = false;
+          count[removed - 'a']--;
+        }
+
+        stack.push(s.charAt(i));
+        added[s.charAt(i) - 'a'] = true;
+      } else {
+        count[s.charAt(i) - 'a']--;
+      }
+    }
+
+    StringBuilder result = new StringBuilder();
+    while (!stack.empty()) {
+      result.append(stack.pop());
+    }
+
+    return result.reverse().toString();
+  }
+}
