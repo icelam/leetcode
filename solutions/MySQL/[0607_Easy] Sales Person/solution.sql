b88e2e9d4bec2ceff1ba01951f70cbf69a5b4c85
Ice Lam <pinky.yy.lam@gmail.com>
2022-04-03 17:40:52 +0800

    feat(mysql): 607 - sales person

---
+++ solutions/MySQL/[0607_Easy] Sales Person/solution.sql
@@ -0,0 +1,6 @@
+SELECT SalesPerson.name FROM SalesPerson
+WHERE SalesPerson.sales_id NOT IN (
+  SELECT Orders.sales_id FROM Orders
+  LEFT JOIN Company ON Orders.com_id = Company.com_id
+  WHERE Company.name = "Red"
+);
