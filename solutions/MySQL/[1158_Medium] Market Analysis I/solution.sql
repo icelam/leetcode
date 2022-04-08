2d102dfe9a73195035ab1013a70312e34a76b02d
Ice Lam <pinky.yy.lam@gmail.com>
2022-04-08 22:19:56 +0800

    feat(mysql): 1158 - market analysis i

---
+++ solutions/MySQL/[1158_Medium] Market Analysis I/solution.sql
@@ -0,0 +1,8 @@
+SELECT
+  Users.user_id AS buyer_id,
+  Users.join_date,
+  IFNULL(COUNT(Orders.item_id), 0) AS orders_in_2019
+FROM Users
+LEFT JOIN Orders
+ON Users.user_id = Orders.buyer_id AND YEAR(Orders.order_date) = "2019"
+GROUP BY Users.user_id;
