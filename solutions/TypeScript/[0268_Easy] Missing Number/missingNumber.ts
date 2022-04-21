2584fd8ebe2c7a056c725de19e762d7a748836eb
Ice Lam <pinky.yy.lam@gmail.com>
2022-04-21 21:09:10 +0800

    refactor(typescript): 268 - missing number

--- solutions/TypeScript/[0268_Easy] Missing Number/missingNumber.ts
+++ solutions/TypeScript/[0268_Easy] Missing Number/missingNumber.ts
@@ -1,6 +1,6 @@
 function missingNumber(nums: number[]): number {
   let accumulator = 0;
-  const arraySum = nums.forEach((n, currentIndex) => {
+  nums.forEach((n, currentIndex) => {
     accumulator += currentIndex + 1;
     accumulator -= n;
   });
