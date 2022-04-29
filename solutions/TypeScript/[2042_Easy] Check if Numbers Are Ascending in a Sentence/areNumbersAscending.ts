24ea59327e6be9afed082a64dfac8478bb687e59
Ice Lam <pinky.yy.lam@gmail.com>
2022-04-30 03:21:24 +0800

    refactor(typescript): 2042 - check if numbers are ascending in a sentence

--- solutions/TypeScript/[2042_Easy] Check if Numbers Are Ascending in a Sentence/areNumbersAscending.ts
+++ solutions/TypeScript/[2042_Easy] Check if Numbers Are Ascending in a Sentence/areNumbersAscending.ts
@@ -1,13 +1,23 @@
 function areNumbersAscending(s: string): boolean {
-  const numbers = s
-    .split(' ')
-    .map((character) => Number(character))
-    .filter((n) => !Number.isNaN(n));
+  const CHAR_CODE_0 = '0'.charCodeAt(0);
+  const CHAR_CODE_9 = '9'.charCodeAt(0);
 
-  for (let i = 0; i < numbers.length; i++) {
-    const previous = i === 0 ? -1 : Number(numbers[i - 1]);
-    if (Number(numbers[i]) <= previous) {
-      return false;
+  const parts: string[] = s.split(' ');
+  let previous = -1;
+
+  for (const p of parts) {
+    if (p.charCodeAt(0) >= CHAR_CODE_0 && p.charCodeAt(0) <= CHAR_CODE_9) {
+      let current = 0;
+
+      for (let i = p.length - 1, j = 1; i >= 0; i--, j *= 10) {
+        current += (p.charCodeAt(i) - CHAR_CODE_0) * j;
+      }
+
+      if (current <= previous) {
+        return false;
+      }
+
+      previous = current;
     }
   }
 
