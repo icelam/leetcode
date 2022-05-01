1bcc2eb27ca53eaa0aa39a3e6656bd70a33585a1
Ice Lam <pinky.yy.lam@gmail.com>
2022-05-01 17:28:33 +0800

    refactor(java): 977 - squares of a sorted array

--- solutions/Java/[0977_Easy] Squares of a Sorted Array/sortedSquares.java
+++ solutions/Java/[0977_Easy] Squares of a Sorted Array/sortedSquares.java
@@ -1,11 +1,19 @@
 class Solution {
   public int[] sortedSquares(int[] nums) {
-    for (int i = 0; i < nums.length; i++) {
-      nums[i] = (int) Math.pow(nums[i], 2);
+    int i = 0;
+    int j = nums.length - 1;
+    int[] result = new int[nums.length];
+
+    for (int k = nums.length - 1; k >= 0; k--) {
+      if (Math.abs(nums[i]) >= Math.abs(nums[j])) {
+        result[k] = (int) Math.pow(nums[i], 2);
+        i++;
+      } else {
+        result[k] = (int) Math.pow(nums[j], 2);
+        j--;
+      }
     }
 
-    Arrays.sort(nums);
-
-    return nums;
+    return result;
   }
 }
