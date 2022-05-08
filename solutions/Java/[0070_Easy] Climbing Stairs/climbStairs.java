6e6f00c168f7884f00491c0367aeb76126cba849
Ice Lam <pinky.yy.lam@gmail.com>
2022-05-08 16:38:06 +0800

    feat(java): 70 - climbing stairs

---
+++ solutions/Java/[0070_Easy] Climbing Stairs/climbStairs.java
@@ -0,0 +1,20 @@
+class Solution {
+  private int[] memo = new int[46];
+
+  public int climbStairs(int n) {
+    if (n == 0) {
+      return 1;
+    }
+
+    if (n < 0) {
+      return 0;
+    }
+
+    if (memo[n] > 0) {
+      return memo[n];
+    }
+
+    memo[n] = climbStairs(n - 1) + climbStairs(n - 2);
+    return memo[n];
+  }
+}
