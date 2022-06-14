fc59b9a4b0add44511779f1c5b298d33c7b05879
Ice Lam <pinky.yy.lam@gmail.com>
2022-06-14 22:37:27 +0800

    feat(java): 583 - delete operation for two strings

---
+++ solutions/Java/[0583_Medium] Delete Operation for Two Strings/minDistance.java
@@ -0,0 +1,23 @@
+class Solution {
+  public int minDistance(String word1, String word2) {
+    int m = word1.length();
+    int n = word2.length();
+    int[][] dp = new int[m + 1][n + 1];
+    char[] c1 = word1.toCharArray();
+    char[] c2 = word2.toCharArray();
+
+    for (int i = 0; i <= m; i++) {
+      for (int j = 0; j <= n; j++) {
+        if (i == 0 || j == 0) {
+          dp[i][j] = i + j;
+        } else if (c1[i - 1] == c2[j - 1]) {
+          dp[i][j] = dp[i - 1][j - 1];
+        } else {
+          dp[i][j] = 1 + Math.min(dp[i - 1][j], dp[i][j - 1]);
+        }
+      }
+    }
+
+    return dp[m][n];
+  }
+}
