cfcefb67ffc2f5028d82b41cc4cec817aba48ce5
Ice Lam <pinky.yy.lam@gmail.com>
2022-07-07 15:28:33 +0800

    feat(java): 509 - fibonacci number

---
+++ solutions/Java/[0509_Easy] Fibonacci Number/fib.java
@@ -0,0 +1,20 @@
+class Solution {
+  private static int[] dp = new int[31];
+
+  public int fib(int n) {
+    if (n == 0) {
+      return 0;
+    }
+
+    if (n == 1) {
+      return 1;
+    }
+
+    if (dp[n] > 0) {
+      return dp[n];
+    }
+
+    dp[n] = fib(n - 1) + fib(n - 2);
+    return dp[n];
+  }
+}
