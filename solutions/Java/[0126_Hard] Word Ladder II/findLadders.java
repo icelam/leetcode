51fbdb418dd5ca7b095268e2d8d6879e68a25ff2
Ice Lam <pinky.yy.lam@gmail.com>
2022-08-15 19:58:16 +0800

    refactor(java): 126 - word ladder ii

--- solutions/Java/[0126_Hard] Word Ladder II/findLadders.java
+++ solutions/Java/[0126_Hard] Word Ladder II/findLadders.java
@@ -1,4 +1,41 @@
 class Solution {
+  private boolean isOneCharDifferernt(String s1, String s2) {
+    if (s1.length() != s2.length()) {
+      return false;
+    }
+
+    char[] s1Chars = s1.toCharArray();
+    char[] s2Chars = s2.toCharArray();
+    int differenceCount = 0;
+
+    for (int j = 0; j < s1Chars.length; j++) {
+      if (s1Chars[j] != s2Chars[j]) {
+        differenceCount++;
+      }
+    }
+
+    return differenceCount == 1;
+  }
+
+  private HashMap<String, HashSet<String>> generateAdjencyList(String beginWord, List<String> wordList) {
+    HashMap<String, HashSet<String>> adjencyList = new HashMap<>();
+    HashSet<String> uniqueWordList = new HashSet<>(wordList);
+    uniqueWordList.add(beginWord);
+
+    for (String word: uniqueWordList) {
+      HashSet<String> neighbour = new HashSet<>();
+      adjencyList.put(word, neighbour);
+
+      for (String candidate: uniqueWordList) {
+        if (isOneCharDifferernt(word, candidate)) {
+          neighbour.add(candidate);
+        }
+      }
+    }
+
+    return adjencyList;
+  }
+
   public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
     List<List<String>> result = new ArrayList<>();
 
@@ -6,7 +43,7 @@ class Solution {
       return result;
     }
 
-    HashSet<String> uniqueWordList = new HashSet<>(wordList);
+    HashMap<String, HashSet<String>> adjencyList = generateAdjencyList(beginWord, wordList);
 
     HashSet<String> visited = new HashSet<>();
     visited.add(beginWord);
@@ -20,9 +57,11 @@ class Solution {
       int batchSize = queue.size();
       HashSet<String> currentVisited = new HashSet<>();
 
+
       for (int i = 0; i < batchSize; i++) {
         List<String> sequence = queue.poll();
-        String previousWord = sequence.get(sequence.size() - 1);
+        int sequenceLength = sequence.size();
+        String previousWord = sequence.get(sequenceLength - 1);
 
         if (previousWord.equals(endWord)) {
           result.add(sequence);
@@ -30,7 +69,11 @@ class Solution {
           continue;
         }
 
-        for (String newWord: uniqueWordList) {
+        if (result.size() > 0 && sequenceLength >= result.get(0).size()) {
+          continue;
+        }
+
+        for (String newWord: adjencyList.get(previousWord)) {
           if (previousWord.length() != newWord.length()) {
             continue;
           }
