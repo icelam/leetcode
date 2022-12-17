1da4de97d94a1034411511e7b066f876c26a044a
Ice Lam <pinky.yy.lam@gmail.com>
2022-12-17 15:04:27 +0800

    feat(java): 101 - symmetric tree

---
+++ solutions/Java/[0101_Easy] Symmetric Tree/isSymmetric.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+  private boolean dfs(TreeNode node1, TreeNode node2) {
+    if (node1 == null && node2 == null) {
+      return true;
+    }
+
+    if (node1 == null || node2 == null) {
+      return false;
+    }
+
+    return node1.val == node2.val && dfs(node1.left, node2.right) && dfs(node1.right, node2.left);
+  }
+
+  public boolean isSymmetric(TreeNode root) {
+    return dfs(root.left, root.right);
+  }
+}
