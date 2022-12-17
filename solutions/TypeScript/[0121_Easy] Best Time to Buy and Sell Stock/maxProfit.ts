fdba6978c9075df40cd5ad176a380075238c9b89
Ice Lam <pinky.yy.lam@gmail.com>
2022-12-17 17:17:21 +0800

    refactor(typescript): 121 - best time to buy and sell stock

--- solutions/TypeScript/[0121_Easy] Best Time to Buy and Sell Stock/maxProfit.ts
+++ solutions/TypeScript/[0121_Easy] Best Time to Buy and Sell Stock/maxProfit.ts
@@ -1,31 +1,14 @@
 function maxProfit(prices: number[]): number {
-  let low: null | number = null;
-  let high: null | number = null;
+  let minPrice = prices[0];
   let possibleMaxProfit = 0;
 
-  function setPossibleProfit() {
-    if (high !== null && low !== null) {
-      if (high - low > possibleMaxProfit) {
-        possibleMaxProfit = high - low;
-      }
-    }
-
-    low = null;
-    high = null;
-  }
-
   for (let i = 0; i < prices.length; i++) {
-    if (low === null || prices[i] <= low) {
-      setPossibleProfit();
-      low = prices[i];
-      continue;
-    }
-
-    if (high === null || prices[i] > high) {
-      high = prices[i];
+    if (prices[i] < minPrice) {
+      minPrice = prices[i];
+    } else if (prices[i] - minPrice > possibleMaxProfit) {
+      possibleMaxProfit = prices[i] - minPrice;
     }
   }
 
-  setPossibleProfit();
   return possibleMaxProfit;
 }
