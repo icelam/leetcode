fe28fe8551a072dbf09e6fd4f72997e2a7b9bca6
Ice Lam <pinky.yy.lam@gmail.com>
2023-01-05 22:51:21 +0800

    refactor(typescript): 394 - decode string

--- solutions/TypeScript/[0394_Medium] Decode String/decodeString.ts
+++ solutions/TypeScript/[0394_Medium] Decode String/decodeString.ts
@@ -1,41 +1,69 @@
 function decodeString(s: string): string {
-  const stack = [];
-  let temp = '';
-  let isPrevioisCharNumber = false;
-  let bracketsOpened = 0;
+  const isDigit = (c: string): boolean => (
+    c.charCodeAt(0) >= '0'.charCodeAt(0) && c.charCodeAt(0) <= '9'.charCodeAt(0)
+  );
 
-  for (const char of s) {
-    if (!isPrevioisCharNumber && !bracketsOpened && temp) {
-      stack.push(temp);
-      temp = '';
-    }
+  const isStartOfRepeatMark = (c: string): boolean => (c === '[');
 
-    if (char.charCodeAt(0) >= '0'.charCodeAt(0) && char.charCodeAt(0) <= '9'.charCodeAt(0)) {
-      isPrevioisCharNumber = true;
-    } else if (char === '[') {
-      bracketsOpened += 1;
-      isPrevioisCharNumber = false;
-    } else if (char === ']') {
-      bracketsOpened -= 1;
-      isPrevioisCharNumber = false;
-    }
+  const isEndOfRepeatMark = (c: string): boolean => (c === ']');
 
-    temp += char;
-  }
+  const isLetter = (c: string): boolean => (
+    !isDigit(c) && !isStartOfRepeatMark(c) && !isEndOfRepeatMark(c)
+  );
 
-  stack.push(temp);
+  const parts: string[] = [];
+  const frequencies: number[] = [];
+  const n = s.length;
+  let i = 0;
 
-  let result = '';
+  while (i < n) {
+    if (isDigit(s[i])) {
+      let occurance = 0;
 
-  for (const part of stack) {
-    if (part.includes('[')) {
-      const numberOfRepeat = Number(part.substring(0, part.indexOf('[')));
-      const substring = part.substring(part.indexOf('[') + 1, part.length - 1);
-      result += decodeString(substring).repeat(numberOfRepeat);
+      while (i < n && isDigit(s[i])) {
+        occurance = occurance * 10 + parseInt(s[i], 10);
+        i++;
+      }
+
+      frequencies.push(occurance);
+    } else if (isStartOfRepeatMark(s[i])) {
+      if (!isLetter(s[i + 1])) {
+        parts.push('');
+      }
+
+      i++;
+    } else if (isEndOfRepeatMark(s[i])) {
+      let repeatedPart = '';
+      const currentPart = parts.pop();
+
+      for (let occurance = (frequencies.pop() ?? 0); occurance > 0; occurance--) {
+        repeatedPart += currentPart;
+      }
+
+      if (parts.length) {
+        const previousPart = parts.pop();
+        repeatedPart = previousPart + repeatedPart;
+      }
+
+      parts.push(repeatedPart);
+      i++;
     } else {
-      result += part.repeat(1);
+      let currentPart = '';
+      const previousChar = i > 0 ? s[i - 1] : ' ';
+
+      while (i < n && isLetter(s[i])) {
+        currentPart += s[i];
+        i++;
+      }
+
+      if (isEndOfRepeatMark(previousChar)) {
+        const previousPart = parts.pop();
+        currentPart += previousPart + currentPart;
+      }
+
+      parts.push(currentPart);
     }
   }
 
-  return result;
+  return parts.pop() ?? '';
 }
