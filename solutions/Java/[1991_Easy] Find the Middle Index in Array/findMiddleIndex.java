8c53d7f88902dccacfd1ce183d73b697a0b6ede7
Ice Lam <pinky.yy.lam@gmail.com>
2023-01-05 20:49:19 +0800

    feat(java): 1991 - find the middle index in array

---
+++ solutions/Java/[1991_Easy] Find the Middle Index in Array/findMiddleIndex.java
@@ -0,0 +1,22 @@
+class Solution {
+  public int findMiddleIndex(int[] nums) {
+    int length = nums.length;
+    int[] prefixSum = new int[length];
+    prefixSum[0] = nums[0];
+
+    for (int i = 1; i < length; i++) {
+      prefixSum[i] = prefixSum[i - 1] + nums[i];
+    }
+
+    for (int i = 0; i < length; i++) {
+      int leftSum = prefixSum[i] - nums[i];
+      int rightSum = prefixSum[length - 1] - prefixSum[i];
+
+      if (leftSum == rightSum) {
+        return i;
+      }
+    }
+
+    return -1;
+  }
+}
