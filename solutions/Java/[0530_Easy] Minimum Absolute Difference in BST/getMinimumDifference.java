08db064494b4828fb6b808067605a9c9b72450ab
Ice Lam <pinky.yy.lam@gmail.com>
2023-02-17 21:44:06 +0800

    feat(java): 530 - minimum absolute difference in bst

---
+++ solutions/Java/[0530_Easy] Minimum Absolute Difference in BST/getMinimumDifference.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+  private Integer previous;
+  private int min;
+
+  public final void inOrder(TreeNode root) {
+    if (root != null) {
+      inOrder(root.left);
+
+      if (previous != null) {
+        min = Math.min(min, root.val - previous);
+      }
+
+      previous = root.val;
+      inOrder(root.right);
+    }
+  }
+
+  public int getMinimumDifference(TreeNode root) {
+    min = Integer.MAX_VALUE;
+    inOrder(root);
+    return min;
+  }
+}
