07b35cdf128794e1536cc861dc0cc768e034b249
Ice Lam <pinky.yy.lam@gmail.com>
2023-02-21 01:18:23 +0800

    feat(java): 628 - maximum product of three numbers

---
+++ solutions/Java/[0628_Easy] Maximum Product of Three Numbers/maximumProduct.java
@@ -0,0 +1,10 @@
+class Solution {
+  public int maximumProduct(int[] nums) {
+    int length = nums.length;
+    Arrays.sort(nums);
+    return Math.max(
+      nums[length - 1] * nums[length - 2] * nums[length - 3],
+      nums[0] * nums[1] * nums[nums.length - 1]
+    );
+  }
+}
