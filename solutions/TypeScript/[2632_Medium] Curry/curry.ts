266c9047374121be668f3fb1489f0fe594059752
Ice Lam <pinky.yy.lam@gmail.com>
2023-04-14 01:02:49 +0800

    feat(typescript): 2632 - curry

---
+++ solutions/TypeScript/[2632_Medium] Curry/curry.ts
@@ -0,0 +1,11 @@
+function curry(fn: Function): Function {
+  return function curried(...args) {
+    if (args.length >= fn.length) {
+      return fn.apply(this, args);
+    }
+
+    return function (...args2) {
+      return curried.apply(this, args.concat(args2));
+    };
+  };
+}
