c7758198c96a1f94cc69830ed94dbcd5137f7f3f
Ice Lam <pinky.yy.lam@gmail.com>
2023-04-15 00:19:07 +0800

    feat(java): 516 - longest palindromic subsequence

---
+++ solutions/Java/[0516_Medium] Longest Palindromic Subsequence/longestPalindromeSubseq.java
@@ -0,0 +1,22 @@
+class Solution {
+  public int longestPalindromeSubseq(String s) {
+    char[] characters = s.toCharArray();
+    int n = characters.length;
+
+    int[][] dp = new int[n][n];
+
+    for (int i = n - 1; i >= 0; i--) {
+      dp[i][i] = 1;
+
+      for (int j = i + 1; j < s.length(); j++) {
+        if (characters[i] == characters[j]) {
+          dp[i][j] = dp[i + 1][j - 1] + 2;
+        } else {
+          dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+        }
+      }
+    }
+
+    return dp[0][n - 1];
+  }
+}
