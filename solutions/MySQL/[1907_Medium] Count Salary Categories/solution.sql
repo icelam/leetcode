a812de70bed309d1e85fde4c68f8f85a6e31086f
Ice Lam <pinky.yy.lam@gmail.com>
2023-05-28 03:15:48 +0800

    feat(mysql): 1907 - count salary categories

---
+++ solutions/MySQL/[1907_Medium] Count Salary Categories/solution.sql
@@ -0,0 +1,23 @@
+(
+  SELECT
+    'Low Salary' AS category ,
+    COUNT(*) AS accounts_count
+  FROM accounts
+  WHERE income < 20000
+)
+UNION
+(
+  SELECT
+    'Average Salary' AS category ,
+    COUNT(*) AS accounts_count
+  FROM accounts
+  WHERE income BETWEEN 20000 and 50000
+)
+UNION
+(
+  SELECT
+    'High Salary' AS category ,
+    COUNT(*) AS accounts_count
+  FROM accounts
+  WHERE income > 50000
+)
