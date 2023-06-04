9d45f13efd6ec89eea3062fe5b8ded2fc6a700ee
Ice Lam <pinky.yy.lam@gmail.com>
2023-06-04 14:17:13 +0800

    feat(java): 547 - number of provinces

---
+++ solutions/Java/[0547_Medium] Number of Provinces/findCircleNum.java
@@ -0,0 +1,64 @@
+class UnionFind {
+  private int[] group;
+  private int[] rank;
+
+  public UnionFind(int size) {
+    group = new int[size];
+    rank = new int[size];
+
+    for (int i = 0; i < size; i++) {
+      group[i] = i;
+    }
+  }
+
+  public int find(int vertex) {
+    if (group[vertex] != vertex) {
+      group[vertex] = find(group[vertex]);
+    }
+    return group[vertex];
+  }
+
+  public boolean union(int vertex1, int vertex2) {
+    int group1 = find(vertex1);
+    int group2 = find(vertex2);
+
+    if (group1 == group2) {
+      return false;
+    }
+
+    if (rank[group1] > rank[group2]) {
+      group[group2] = group1;
+    } else if (rank[group1] < rank[group2]) {
+      group[group1] = group2;
+    } else {
+      group[group1] = group2;
+      rank[group2] += 1;
+    }
+
+    return true;
+  }
+}
+
+class Solution {
+  public int findCircleNum(int[][] isConnected) {
+    int n = isConnected.length;
+
+    UnionFind uf = new UnionFind(n);
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (isConnected[i][j] == 1) {
+          uf.union(i, j);
+        }
+      }
+    }
+
+    HashSet<Integer> groups = new HashSet<>();
+
+    for (int i = 0; i < n; i++) {
+      groups.add(uf.find(i));
+    }
+
+    return groups.size();
+  }
+}
