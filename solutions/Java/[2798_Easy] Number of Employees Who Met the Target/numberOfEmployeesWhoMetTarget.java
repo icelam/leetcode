a229b59644f5d67957e6893023173df1f13dbeb7
Ice Lam <pinky.yy.lam@gmail.com>
2023-08-03 15:11:05 +0800

    feat(java): 2798 - number of employees who met the target

---
+++ solutions/Java/[2798_Easy] Number of Employees Who Met the Target/numberOfEmployeesWhoMetTarget.java
@@ -0,0 +1,13 @@
+class Solution {
+  public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+    int result = 0;
+
+    for (int h: hours) {
+      if (h >= target) {
+        result++;
+      }
+    }
+
+    return result;
+  }
+}
