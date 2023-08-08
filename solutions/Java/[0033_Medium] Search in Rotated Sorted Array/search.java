6d0128533c8cfc68543a6b2b327b3ae24697a19b
Ice Lam <pinky.yy.lam@gmail.com>
2023-08-08 22:23:27 +0800

    feat(java): 33 - search in rotated sorted array

---
+++ solutions/Java/[0033_Medium] Search in Rotated Sorted Array/search.java
@@ -0,0 +1,44 @@
+class Solution {
+  private int findPrivot(int[] arr) {
+    int left = 0;
+    int right = arr.length - 1;
+    int rightMostNum = arr[right];
+
+    while (left <= right) {
+      int middle = left + (right - left) / 2;
+      if (arr[middle] > rightMostNum) {
+        left = middle + 1;
+      } else {
+        right = middle - 1;
+      }
+    }
+
+    return left;
+  }
+
+  private int binarySearch(int[] arr, int target, int left, int right) {
+    while (left <= right) {
+      int middle = left + (right - left) / 2;
+
+      if (arr[middle] == target) {
+        return middle;
+      }
+
+      if (arr[middle] < target) {
+        left = middle + 1;
+      } else {
+        right = middle - 1;
+      }
+    }
+
+    return -1;
+  }
+
+  public int search(int[] nums, int target) {
+    int n = nums.length;
+    int privotIndex = findPrivot(nums);
+    int left = target > nums[n - 1] ? 0 : privotIndex;
+    int right = target > nums[n - 1] ? privotIndex - 1 : n - 1;
+    return binarySearch(nums, target, left, right);
+  }
+}
