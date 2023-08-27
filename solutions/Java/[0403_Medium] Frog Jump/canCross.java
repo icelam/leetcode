91fed8d376badc28888e0a9abeed4988fe18d0e1
Ice Lam <pinky.yy.lam@gmail.com>
2023-08-28 02:27:02 +0800

    feat(java): 403 - frog jump

---
+++ solutions/Java/[0403_Medium] Frog Jump/canCross.java
@@ -0,0 +1,35 @@
+class Solution {
+  public boolean canCross(int[] stones) {
+    int n = stones.length;
+    boolean[][] dp = new boolean[n][n];
+    dp[0][0] = true;
+
+    for (int i = 1; i < n; i++) {
+      if (stones[i] - stones[i - 1] > i) {
+        return false;
+      }
+    }
+
+    for (int i = 1; i < n; i++) {
+      // Try all previois stone
+      for (int j = i - 1; j >= 0; j--) {
+        // last jump distance
+        int k = stones[i] - stones[j];
+
+        if (k > j + 1) {
+          break;
+        }
+
+        // Frog's next jump must be either k - 1, k, or k + 1 units.
+        dp[i][k] = dp[j][k - 1] || dp[j][k] || dp[j][k + 1];
+
+        // Reached last stone
+        if (i == n - 1 && dp[i][k]) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+}
