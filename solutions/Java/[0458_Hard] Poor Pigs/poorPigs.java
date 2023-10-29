f8361e81d4ad59f1198ff5627896fbb075056bb1
Ice Lam <pinky.yy.lam@gmail.com>
2023-10-29 16:38:53 +0800

    refactor(java): 458 - poor pigs

--- solutions/Java/[0458_Hard] Poor Pigs/poorPigs.java
+++ solutions/Java/[0458_Hard] Poor Pigs/poorPigs.java
@@ -1,5 +1,8 @@
 class Solution {
   public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
-    return (int) Math.ceil(Math.log(buckets) / Math.log(minutesToTest / minutesToDie + 1));
+    // Updates on 2023-10-29:
+    // New test case buckets = 125, minutesToDie = 1, minutesToTest = 4 will not pass
+    // if using Math.log() instead of Math.log10 due to precision and rounding issues
+    return (int) Math.ceil(Math.log10(buckets) / Math.log10(minutesToTest / minutesToDie + 1));
   }
 }
