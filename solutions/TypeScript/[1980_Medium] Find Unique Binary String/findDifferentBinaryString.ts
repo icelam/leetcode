2bdfa7b2ff376136573da15352de0dc3a1a1adfb
Ice Lam <pinky.yy.lam@gmail.com>
2023-11-16 21:10:05 +0800

    refactor(typescript): 1980 - find unique binary string

--- solutions/TypeScript/[1980_Medium] Find Unique Binary String/findDifferentBinaryString.ts
+++ solutions/TypeScript/[1980_Medium] Find Unique Binary String/findDifferentBinaryString.ts
@@ -1,21 +1,14 @@
 function findDifferentBinaryString(nums: string[]): string {
-  const numCount: Record<string, number> = {};
+  nums.sort();
 
-  for (const n of nums) {
-    const decimal = parseInt(n, 2);
+  const n = nums.length;
+  let missingNum = 0;
 
-    numCount[decimal] = (numCount[decimal] ?? 0) + 1;
+  while (missingNum < n && parseInt(nums[missingNum], 2) === missingNum) {
+    missingNum++;
   }
 
-  for (let i = 0; i < Math.pow(2, 16); i++) {
-    if (!numCount[i]) {
-      const binaryString = i.toString(2);
-
-      return binaryString.length >= nums.length
-        ? binaryString
-        : new Array(nums.length - binaryString.length + 1).join('0') + binaryString;
-    }
-  }
-
-  return new Array(nums.length + 1).join('0');
+  const missingBinary = missingNum.toString(2);
+  const zeroPadLength = n - missingBinary.length;
+  return '0'.repeat(zeroPadLength) + missingBinary;
 }
