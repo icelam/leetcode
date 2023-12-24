0dd6f75742f64193c6d3b1049508f2083011ac08
Ice Lam <pinky.yy.lam@gmail.com>
2023-12-24 22:30:09 +0800

    feat(java): 1979 - find greatest common divisor of array

---
+++ solutions/Java/[1979_Easy] Find Greatest Common Divisor of Array/findGCD.java
@@ -0,0 +1,17 @@
+class Solution {
+  private int gcd(int a, int b) {
+    return b == 0 ? a : gcd(b, a % b);
+  }
+
+  public int findGCD(int[] nums) {
+    int min = Integer.MAX_VALUE;
+    int max = Integer.MIN_VALUE;
+
+    for (int n: nums) {
+      min = Math.min(min, n);
+      max = Math.max(max, n);
+    }
+
+    return gcd(min, max);
+  }
+}
