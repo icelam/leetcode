1f6c17ae66d1238749975227f85abb792b16ece1
Ice Lam <pinky.yy.lam@gmail.com>
2024-03-20 02:58:27 +0800

    feat(java): 836 - rectangle overlap

---
+++ solutions/Java/[0836_Easy] Rectangle Overlap/isRectangleOverlap.java
@@ -0,0 +1,10 @@
+class Solution {
+  public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+    return (
+      // Check if X axis overlapped
+      !(rec1[2] <= rec2[0] || rec2[2] <= rec1[0])
+      // Check if Y axis overlapped
+      && !(rec1[3] <= rec2[1] || rec2[3] <= rec1[1])
+    );
+  }
+}
