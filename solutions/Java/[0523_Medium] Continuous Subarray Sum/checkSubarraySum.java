ee514836e8c256f8f91e2b8bdbfe34ca29182310
Ice Lam <pinky.yy.lam@gmail.com>
2024-06-08 16:57:02 +0800

    docs(java): 523 - continuous subarray sum

--- solutions/Java/[0523_Medium] Continuous Subarray Sum/checkSubarraySum.java
+++ solutions/Java/[0523_Medium] Continuous Subarray Sum/checkSubarraySum.java
@@ -8,9 +8,21 @@ class Solution {
     for (int i = 0; i < nums.length; i++) {
       sum += nums[i];
 
+      // Finding the same remainder is equivalent to finding a continuous prefix sum that is a multiple of k.
+      // Assume sum(abc) % k == sum(abcdef) % k == rem, then they satisfy the following properties:
+      // a + b + c = x * k + rem
+      // a + b + c + d + e + f = y * k + rem
+      //
+      // sum(abcdef) - sum(abc)
+      // = (y * k + rem) - (x * k + rem)
+      // = y * k - x * k
+      // = (y - x) * k
+      //
+      // We have thus found a continuous subarray whose sum is a multiple of k.
       if (!map.containsKey(sum % k)) {
         map.put(sum % k, i + 1);
       } else if (map.get(sum % k) < i) {
+        // Only consider subarray length >= 2
         return true;
       }
     }
