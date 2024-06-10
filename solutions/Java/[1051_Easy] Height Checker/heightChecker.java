f083b2d216c4fef0a03954214a9da762654880f7
Ice Lam <pinky.yy.lam@gmail.com>
2024-06-10 17:40:26 +0800

    feat(java): 1051 - height checker

---
+++ solutions/Java/[1051_Easy] Height Checker/heightChecker.java
@@ -0,0 +1,22 @@
+class Solution {
+  public int heightChecker(int[] heights) {
+    int n = heights.length;
+    int[] expected = new int[n];
+
+    for (int i = 0; i < n; i++) {
+      expected[i] = heights[i];
+    }
+
+    Arrays.sort(expected);
+
+    int result = 0;
+
+    for (int i = 0; i < n; i++) {
+      if (heights[i] != expected[i]) {
+        result++;
+      }
+    }
+
+    return result;
+  }
+}
