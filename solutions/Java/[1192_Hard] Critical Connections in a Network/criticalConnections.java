f40e590ddaa20e7c2890c56878cbf72d95ea11c6
Ice Lam <pinky.yy.lam@gmail.com>
2024-06-29 14:25:38 +0800

    refactor(java): 1192 - critical connections in a network

--- solutions/Java/[1192_Hard] Critical Connections in a Network/criticalConnections.java
+++ solutions/Java/[1192_Hard] Critical Connections in a Network/criticalConnections.java
@@ -1,18 +1,18 @@
 class Solution {
-  private Map<Integer, List<Integer>> adjencyList;
+  private Map<Integer, List<Integer>> adjacencyList;
   private int[] lowlink;
   private List<List<Integer>> result;
 
-  public void generateAdjencyList(List<List<Integer>> connections) {
+  public void generateAdjacencyList(List<List<Integer>> connections) {
     for (List<Integer> edge: connections) {
       int node1 = edge.get(0);
       int node2 = edge.get(1);
 
-      adjencyList.putIfAbsent(node1, new ArrayList<>());
-      adjencyList.get(node1).add(node2);
+      adjacencyList.putIfAbsent(node1, new ArrayList<>());
+      adjacencyList.get(node1).add(node2);
 
-      adjencyList.putIfAbsent(node2, new ArrayList<>());
-      adjencyList.get(node2).add(node1);
+      adjacencyList.putIfAbsent(node2, new ArrayList<>());
+      adjacencyList.get(node2).add(node1);
     }
   }
 
@@ -20,7 +20,7 @@ class Solution {
     if (lowlink[node] == -1) {
       lowlink[node] = index;
 
-      for (int neighbor: adjencyList.get(node)) {
+      for (int neighbor: adjacencyList.get(node)) {
         if (neighbor == parent) {
           continue;
         }
@@ -41,8 +41,8 @@ class Solution {
   public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
     result = new ArrayList<>();
 
-    adjencyList = new HashMap<Integer, List<Integer>>();
-    generateAdjencyList(connections);
+    adjacencyList = new HashMap<Integer, List<Integer>>();
+    generateAdjacencyList(connections);
 
     lowlink = new int[n];
     Arrays.fill(lowlink, -1);
