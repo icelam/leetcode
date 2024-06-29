893639770598105c424019abe827dfc5c1902320
Ice Lam <pinky.yy.lam@gmail.com>
2024-06-29 14:25:21 +0800

    refactor(java): 863 - all nodes distance k in binary tree

--- solutions/Java/[0863_Medium] All Nodes Distance K in Binary Tree/distanceK.java
+++ solutions/Java/[0863_Medium] All Nodes Distance K in Binary Tree/distanceK.java
@@ -1,46 +1,46 @@
 class Solution {
-  private void buildGraph(Map<Integer, List<Integer>> adjencyList, TreeNode currentNode, TreeNode parentNode) {
+  private void buildGraph(Map<Integer, List<Integer>> adjacencyList, TreeNode currentNode, TreeNode parentNode) {
     if (currentNode == null) {
       return;
     }
 
-    adjencyList.putIfAbsent(currentNode.val, new ArrayList<>());
+    adjacencyList.putIfAbsent(currentNode.val, new ArrayList<>());
 
     if (parentNode != null) {
-      adjencyList.get(currentNode.val).add(parentNode.val);
+      adjacencyList.get(currentNode.val).add(parentNode.val);
     }
 
     if (currentNode.left != null) {
-      adjencyList.get(currentNode.val).add(currentNode.left.val);
-      buildGraph(adjencyList, currentNode.left, currentNode);
+      adjacencyList.get(currentNode.val).add(currentNode.left.val);
+      buildGraph(adjacencyList, currentNode.left, currentNode);
     }
 
     if (currentNode.right != null) {
-      adjencyList.get(currentNode.val).add(currentNode.right.val);
-      buildGraph(adjencyList, currentNode.right, currentNode);
+      adjacencyList.get(currentNode.val).add(currentNode.right.val);
+      buildGraph(adjacencyList, currentNode.right, currentNode);
     }
   }
 
-  private void dfs(Map<Integer, List<Integer>> adjencyList, int currentNode, int k, List<Integer> result, HashSet<Integer> visited) {
+  private void dfs(Map<Integer, List<Integer>> adjacencyList, int currentNode, int k, List<Integer> result, HashSet<Integer> visited) {
     if (k == 0) {
       result.add(currentNode);
     }
 
     visited.add(currentNode);
 
-    for (int neighborNode: adjencyList.get(currentNode)) {
+    for (int neighborNode: adjacencyList.get(currentNode)) {
       if (!visited.contains(neighborNode)) {
-        dfs(adjencyList, neighborNode, k - 1, result, visited);
+        dfs(adjacencyList, neighborNode, k - 1, result, visited);
       }
     }
   }
 
   public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-    Map<Integer, List<Integer>> adjencyList = new HashMap<>();
-    buildGraph(adjencyList, root, null);
+    Map<Integer, List<Integer>> adjacencyList = new HashMap<>();
+    buildGraph(adjacencyList, root, null);
 
     List<Integer> result = new ArrayList<>();
-    dfs(adjencyList, target.val, k, result, new HashSet<Integer>());
+    dfs(adjacencyList, target.val, k, result, new HashSet<Integer>());
 
     return result;
   }
