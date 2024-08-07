1bfaa10032bae084face91500824b10b9c40090b
Ice Lam <pinky.yy.lam@gmail.com>
2024-08-07 23:59:47 +0800

    feat(java): 273 - integer to english words

---
+++ solutions/Java/[0273_Hard] Integer to English Words/numberToWords.java
@@ -0,0 +1,49 @@
+class Solution {
+  private static String[] baseNumbers = new String[] {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+  private static String[] multipleOfTenNumbers = new String[] {"Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety", "Hundred"};
+  private static String[] units = new String[] {"", "Thousand", "Million", "Billion"};
+
+  public String numberToWords(int num) {
+    if (num == 0) {
+      return baseNumbers[0];
+    }
+
+    StringBuilder builder = new StringBuilder();
+    int unitIndex = 0;
+
+    while (num > 0) {
+      int currentPart = num % 1000;
+
+      if (currentPart > 0) {
+        builder.insert(0, " ");
+        builder.insert(0, units[unitIndex]);
+
+        int hundredDigit = currentPart / 100;
+        int remainingDigits = currentPart % 100;
+
+        if (remainingDigits > 0 && remainingDigits < 20) {
+          builder.insert(0, " ");
+          builder.insert(0, baseNumbers[remainingDigits]);
+        } else if (remainingDigits >= 20) {
+          if (remainingDigits % 10 > 0) {
+            builder.insert(0, " ");
+            builder.insert(0, baseNumbers[remainingDigits % 10]);
+          }
+
+          builder.insert(0, " ");
+          builder.insert(0, multipleOfTenNumbers[remainingDigits / 10 - 2]);
+        }
+
+        if (hundredDigit > 0) {
+          builder.insert(0, " Hundred ");
+          builder.insert(0, baseNumbers[hundredDigit]);
+        }
+      }
+
+      unitIndex++;
+      num /= 1000;
+    }
+
+    return builder.toString().trim();
+  }
+}
