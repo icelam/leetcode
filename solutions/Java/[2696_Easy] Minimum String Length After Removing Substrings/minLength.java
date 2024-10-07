6da685f768aa2c0f1ab4838ac03d52fa00b49325
Ice Lam <pinky.yy.lam@gmail.com>
2024-10-07 10:30:10 +0800

    feat(java): 2696 - minimum string length after removing substrings

---
+++ solutions/Java/[2696_Easy] Minimum String Length After Removing Substrings/minLength.java
@@ -0,0 +1,21 @@
+class Solution {
+  public int minLength(String s) {
+    Stack<Character> stack = new Stack<>();
+
+    for (char c: s.toCharArray()) {
+      if (stack.isEmpty()) {
+        stack.push(c);
+        continue;
+      }
+
+      if ((c == 'B' && stack.peek() == 'A') || (c == 'D' && stack.peek() == 'C')) {
+        stack.pop();
+        continue;
+      }
+
+      stack.push(c);
+    }
+
+    return stack.size();
+  }
+}
